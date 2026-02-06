cf478c14bad15a0a374e84154a216654dd9d89e0
Sampan Verma <himself@samlovescoding.com>
2026-02-06 09:25:27 +0530

    deploy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -29,6 +29,8 @@
 
 @task('frontend')
     echo "Building frontend assets..."
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
     cd {{ $directory }}
     npm ci
     npm run build
